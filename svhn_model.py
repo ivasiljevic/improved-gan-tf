0997f81d2bdd424d4907a6c9ca2c8fd7a6e05e5d
Igor Vasiljevic <ivas@slurm.ttic.edu>
2016-11-21 09:14:35 -0600

    update arch

--- svhn_model.py
+++ svhn_model.py
@@ -2,15 +2,12 @@ import tensorflow as tf
 import numpy as np
 from ops import *
 
-batch_size = 32
-sample_size = 32
+batch_size = 100
+sample_size = 100
 
 z_dim = 100
 gf_dim = 64
 df_dim = 64
-
-learning_rate = 0.0002
-beta1 = 0.5
 
 d_bn1 = batch_norm(name='d_bn1')
 d_bn2 = batch_norm(name='d_bn2')
@@ -18,9 +15,6 @@ d_bn3 = batch_norm(name='d_bn3')
 d_bn4 = batch_norm(name='d_bn4')
 d_bn5 = batch_norm(name='d_bn5')
 d_bn6 = batch_norm(name='d_bn6')
-d_bn7 = batch_norm(name='d_bn7')
-d_bn8 = batch_norm(name='d_bn8')
-d_bn9 = batch_norm(name='d_bn9')
 
 g_bn0 = batch_norm(name='g_bn0')
 g_bn1 = batch_norm(name='g_bn1')
@@ -30,7 +24,6 @@ g_bn3 = batch_norm(name='g_bn3')
 def discriminator(image, reuse=False):
     if reuse:
         tf.get_variable_scope().reuse_variables()
-
     im0 = tf.reshape(image, [-1, 32*32*3])
     drop0 =  tf.nn.dropout(im0, 0.2)
     im1 = tf.reshape(drop0, [-1, 32, 32, 3])
@@ -43,18 +36,20 @@ def discriminator(image, reuse=False):
     h5 = lrelu(conv2d(h4, df_dim*2,name='d_h5_conv'))
     h5_2 = tf.nn.dropout(h5,0.5)
     h6 = lrelu(conv2d(h5_2, df_dim*2, name='d_h6_conv'))
+    h6_2 = tf.nn.avg_pool(h6,ksize=[1, 2, 2, 1],strides=[1, 2, 2, 1], padding='VALID') 
     #h6_2 = tf.nn.avg_pool(h6,ksize=[1,2,2,1],strides=[1,2,2,1],padding="SAME")
-    h6_2 = lrelu(linear(tf.reshape(h6, [batch_size, -1]), 1024, 'd_h2_lin'))
-    h7 = linear(h6_2,10, 'd_h3_lin')
-    #h7 = linear(tf.reshape(h6_2, [batch_size, -1]), 10, 'd_h3_lin')
+    #h6_3 = lrelu(linear(tf.reshape(h6_2, [batch_size, -1]), 1024, 'd_h2_lin'))
+    #h6_4 = tf.nn.dropout(h6_3,0.5)
+   # h7 = linear(h6_3,10, 'd_h3_lin')
+    h7 = linear(tf.reshape(h6_2, [batch_size, -1]), 10, 'd_h3_lin')
     Z = tf.reduce_sum(tf.exp(h7),1)
-    return tf.nn.sigmoid(Z/(Z+1)), Z, tf.nn.softmax(h7)
+    feat_mat = h5
+    return tf.nn.sigmoid(Z/(Z+1)), Z, tf.nn.softmax(h7), feat_mat
 
 def generator(z):
     z_ = linear(z, gf_dim*8*4*4, 'g_h0_lin')
-    h0 = tf.nn.relu(g_bn0(tf.reshape(z_, [-1, 4, 4, gf_dim * 8])))
-    h1 = tf.nn.relu(g_bn1(deconv2d(h0, [batch_size, 8, 8, gf_dim*4], name='g_h1')))
-    h2 = tf.nn.relu(g_bn2(deconv2d(h1, [batch_size, 16, 16, gf_dim*2], name='g_h2')))
+    h0 = g_bn0(tf.nn.relu(tf.reshape(z_, [-1, 4, 4, gf_dim * 8])))
+    h1 = g_bn1(tf.nn.relu(deconv2d(h0, [batch_size, 8, 8, gf_dim*4], name='g_h1')))
+    h2 = g_bn2(tf.nn.relu(deconv2d(h1, [batch_size, 16, 16, gf_dim*2], name='g_h2')))
     h4 = deconv2d(h2, [batch_size, 32, 32, 3], name='g_h3')
-
     return tf.nn.tanh(h4)
--- svhn_train.py
+++ svhn_train.py
@@ -16,9 +16,8 @@ batch = tf.Variable(0)
 phase_train = tf.placeholder(tf.bool, name='phase_train')
 ###############################################################################
 
-batch_size = 32
-image_size = 108
-sample_size = 32
+batch_size = 100
+sample_size = 100
 image_shape = [32, 32, 3]
 
 z_dim = 100
@@ -27,6 +26,7 @@ gf_dim = 64
 df_dim = 64
 
 learning_rate = 0.0003
+#learning_rate = 0.003
 beta1 = 0.5
 
 images = tf.placeholder(tf.float32, [batch_size] + image_shape, name='real_images')
@@ -34,17 +34,17 @@ sample_images= tf.placeholder(tf.float32, [sample_size] + image_shape, name='sam
 z = tf.placeholder(tf.float32, [None, z_dim], name='z')
 
 G = generator(z)
-D, D_logits, predict = discriminator(images)
-D_, D_logits_,predict_ = discriminator(G, reuse=True)
+D, D_logits, predict, feat = discriminator(images)
+D_, D_logits_,predict_,feat_ = discriminator(G, reuse=True)
 
 error = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(predict, y_))
 
 d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(D_logits, tf.ones_like(D)))
 d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(D_logits_, tf.zeros_like(D_)))
-d_loss = d_loss_real + d_loss_fake + error
+d_loss = d_loss_real + d_loss_fake #+ error
 
-g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(D_logits_, tf.ones_like(D_)))
-
+#g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(D_logits_, tf.ones_like(D_)))
+g_loss = tf.nn.l2_loss(feat - feat_)
 t_vars = tf.trainable_variables()
 
 d_vars = [var for var in t_vars if 'd_' in var.name]
@@ -65,33 +65,34 @@ saver = tf.train.Saver()
 if os.path.isfile("model.ckpt"):
     saver.restore(sess, "model.ckpt")
     print("Session restored")
-iterations = 2200
+iterations = 700
+iterations = 200
 
 count = 0
-epoch = 20 
+epoch = 40 
 for k in range(epoch):
     im_id = 0
     for i in range(iterations): #epochs
         batch_images = train_X[im_id : im_id+batch_size ,:]
         labels = train_Y[im_id : im_id + batch_size ,:]
-        batch_z = np.random.uniform(-1, 1, [batch_size, z_dim]).astype(np.float32)
-
-        # Update D network
-        sess.run([d_optim], feed_dict={images: batch_images, z: batch_z, y_: labels})
-        # Update G network
-        #sess.run([g_optim], feed_dict={z: batch_z})
-        # Run g_optim twice to make sure that d_loss does not go to zero (different from paper)
-        sess.run([g_optim], feed_dict={z: batch_z})
-        errD_fake = d_loss_fake.eval({z: batch_z}, session=sess)
+        batch_z = np.random.uniform(0, 1, [batch_size, z_dim]).astype(np.float32)
+        errD_fake = d_loss_fake.eval({z: batch_z,images: batch_images}, session=sess)
         errD_real = d_loss_real.eval({images: batch_images}, session=sess)
-        errG = g_loss.eval({z: batch_z}, session=sess)
-        #print("Error of D: %.8f",errD_real+errD_fake)
+        errG = g_loss.eval({z: batch_z,images: batch_images}, session=sess)
+        errDisc = error.eval({z: batch_z, images:batch_images, y_:labels},session=sess)
+
+        sess.run([d_optim], feed_dict={images: batch_images, z: batch_z, y_: labels})
+        sess.run([g_optim], feed_dict={images: batch_images, z: batch_z, y_: labels})
+
         count = count + 1
         im_id = im_id + batch_size
-        if count % 1000 == 0: 
+        if i % 100 == 0: 
             print("Acc: ",sess.run(accuracy, feed_dict = {images: batch_images, z:batch_z, y_: labels}))
-            print("Image #: ", count)
-    if k % 5 == 0:
+            print("Iter #: ", i)
+            print("Disc error: ", errD_real + errD_fake)
+            print("Gen error: ", errG)
+            print("Class error: ", errDisc)
+    if k % 2 == 0:
         print("On iteration: " + str(k))
         G_samples = sess.run(G, feed_dict = {z : batch_z})
         save_plot(G_samples,k)
